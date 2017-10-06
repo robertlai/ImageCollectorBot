2ee11c98bc111ab9282c931db5fb00d72b0a414b
Robert Lai <rcylai@hotmail.com>
2017-10-05 20:15:26 -0400

    Update collections data format

--- src/data.js
+++ src/data.js
@@ -7,62 +7,75 @@ const BACKUP_DIR = 'backup/';
 var writeLock = 0;
 
 const Data = {
-	defaultCollection: 'Default',
-	collections: {},
-	permissions: {},
-	blacklist: {},
-	currentMonth: '',
+  defaultCollection: 'Default',
+  collections: {
+    Default: {
+      albums: {},
+      announceChannels: {},
+      features: {
+        imgur: false,
+        leaderboard: false
+      },
+      interval: 1000,
+      inChannels: {},
+      outChannels: {},
+      scores: {}
+    }
+  },
+  permissions: {},
+  blacklist: {},
+  currentMonth: '',
 
-	loadData(callback) {
-		Logger.log('Loading data...')
-		fs.readFile(DATA_FILE_NAME, (err, fileContent) => {
-			if (err) {
-				Logger.error(err)
-				Logger.log('Failed to load data.')
-			} else {
-				try {
-					const fileData = JSON.parse(fileContent)
-					Object.assign(this, fileData)
-					Logger.log('Successfully loaded data')
-					this.writeBackup()
-				} catch (err) {
-					Logger.error(err)
-					Logger.log('Failed to parse data file.')
-				}
-			}
-			callback()
-		})
-	},
+  loadData(callback) {
+    Logger.log('Loading data...')
+    fs.readFile(DATA_FILE_NAME, (err, fileContent) => {
+      if (err) {
+        Logger.error(err)
+        Logger.log('Failed to load data.')
+      } else {
+        try {
+          const fileData = JSON.parse(fileContent)
+          Object.assign(this, fileData)
+          Logger.log('Successfully loaded data')
+          this.writeBackup()
+        } catch (err) {
+          Logger.error(err)
+          Logger.log('Failed to parse data file.')
+        }
+      }
+      callback()
+    })
+  },
 
-	writeData() {
-		if (writeLock) {
-			Logger.log('File is locked.')
-			Logger.log('Retrying...')
-			setTimeout(this.writeData, 1000)
-		} else {
-			Logger.log('Writing data...')
-			try {
-				const fileContent = JSON.stringify(this)
-				writeLock++
-				fs.writeFile(DATA_FILE_NAME, fileContent, (err) => {
-					if (err) {
-						Logger.error(err)
-						Logger.log('Failed to write data.')
-					} else {
-						Logger.log('Successfully wrote data.')
-					}
-					writeLock--
-				})
-			} catch (err) {
-				Logger.error(err)
-				Logger.log('Failed to serialize data.')
-			}
-		}
-	},
+  writeData() {
+    if (writeLock) {
+      Logger.log('File is locked.')
+      Logger.log('Retrying...')
+      setTimeout(this.writeData, 1000)
+    } else {
+      Logger.log('Writing data...')
+      try {
+        const fileContent = JSON.stringify(this)
+        writeLock++
+        fs.writeFile(DATA_FILE_NAME, fileContent, (err) => {
+          if (err) {
+            Logger.error(err)
+            Logger.log('Failed to write data.')
+          } else {
+            Logger.log('Successfully wrote data.')
+          }
+          writeLock--
+        })
+      } catch (err) {
+        Logger.error(err)
+        Logger.log('Failed to serialize data.')
+      }
+    }
+  },
 
   writeBackup() {
-  	fs.mkdir(BACKUP_DIR, (err) => {
-  		let dir = BACKUP_DIR
+    fs.mkdir(BACKUP_DIR, (err) => {
+      let dir = BACKUP_DIR
       if (err && err.code !== 'EEXIST') {
         Logger.error(err)
         Logger.log('Failed to create backup directory.')
@@ -82,19 +95,3 @@ const Data = {
 }
 
 export default Data
-
-/*
-var Data = {
-	inChannels: [],
-	outChannels: [],
-	users: [],
-	admin: [],
-	blacklist: [],
-	currentMonth: '',
-	upload: false,
-	albums: {},
-	interval: 1000,
-	trackScores: false,
-	scores: {},
-
-	*/
